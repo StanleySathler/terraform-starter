c8bd577f7ce621774345a94c0af9085cd028e356
StanleySathler <stanleysathlerpinto@gmail.com>
2024-03-06 19:53:27 -0300

    Cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,3 @@
-# General config for Terraform
-# terraform {
-#   required_providers {
-#     docker = {
-#       source  = "kreuzwerker/docker" # Provider source
-#       version = "~> 3.0.1"           # Provider version, omit to install latest
-#     }
-#   }
-# }
-
-# Config for the provider
-# provider "docker" {}
-
-# resource "docker_image" "nginx" {
-#   name         = "nginx"
-#   keep_locally = false
-# }
-
-# resource "docker_container" "nginx" {
-#   image = docker_image.nginx.image_id
-#   name  = var.container_name
-
-#   ports {
-#     internal = 80
-#     external = 8080
-#   }
-# }
-
-
 # Configure the Google Cloud provider
 provider "google" {
   credentials = file("gcp-service-account.json") # TODO: Check a safer approach, perhaps Workload Identity Federation?
